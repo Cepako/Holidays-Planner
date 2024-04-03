49e21cf7a47d729c82572e444d245b3d1d97c7ff
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-04-03 23:39:46 +0200

    add data fetching and generating carts in main tag and style it

--- js/script.js
+++ js/script.js
@@ -9,3 +9,39 @@ const animateBars = () => {
 document
   .querySelector('.navbar-toggler')
   .addEventListener('click', animateBars);
+
+//data fetching
+
+const fetchData = async (passedFunction) => {
+  const response = await fetch('../places.json');
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const fetchedData = await response.json();
+
+  passedFunction(fetchedData);
+};
+
+//generating places in homepage
+
+const generatePlaces = (data) => {
+  const container = document.querySelector('.trending-places');
+  for (let i = 0; i < 4; i++) {
+    const cart = document.createElement('div');
+    cart.classList.add('cart');
+    cart.setAttribute('id', data[i].id);
+    const img = document.createElement('img');
+    img.src = `./images/${data[i].image.src}`;
+    img.alt = data[i].image.alt;
+    const title = document.createElement('h3');
+    title.textContent = data[i].title;
+    cart.appendChild(img);
+    cart.appendChild(title);
+    container.appendChild(cart);
+  }
+};
+
+fetchData(generatePlaces);
