2688c036025586d0c6fb9b590d354aa87b165ffa
Sebastian Dragan <sebastian.dragan305@gmail.com>
2024-04-05 16:36:11 +0200

    add generatingAvailable places and openForm fucntions

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ const fetchData = async (passedFunction) => {
 
 //generating places in homepage
 
-const generatePlaces = (data) => {
+const generateTrendingPlaces = (data) => {
   const container = document.querySelector('.trending-places');
   for (let i = 0; i < 4; i++) {
     const cart = document.createElement('div');
@@ -47,4 +47,56 @@ const generatePlaces = (data) => {
   }
 };
 
-fetchData(generatePlaces);
+//modal and form
+
+const openForm = ({ title, image }) => {
+  const modal = document.querySelector('.modal-form');
+  modal.innerHTML = '';
+  const modalImage = document.createElement('img');
+  modalImage.src = `../images/${image.src}`;
+  modalImage.alt = image.alt;
+  const modalTitle = document.createElement('h3');
+  modalTitle.textContent = title;
+  const closeModal = document.createElement('button');
+  closeModal.textContent = 'Close';
+  closeModal.addEventListener('click', () => modal.close());
+  modal.appendChild(modalImage);
+  modal.appendChild(modalTitle);
+  modal.appendChild(closeModal);
+
+  modal.showModal();
+};
+
+const generateAvailablePlaces = (data) => {
+  const container = document.querySelector('.places-container');
+  for (let i = 0; i < data.length; i++) {
+    const cart = document.createElement('div');
+    cart.classList.add('cart');
+    cart.setAttribute('id', data[i].id);
+    const img = document.createElement('img');
+    img.src = `../images/${data[i].image.src}`;
+    img.alt = data[i].image.alt;
+    const title = document.createElement('h3');
+    title.textContent = data[i].title;
+    const description = document.createElement('p');
+    description.textContent = data[i].short_description;
+    const button = document.createElement('button');
+    button.textContent = 'Add to List';
+    button.addEventListener('click', () => openForm(data[i]));
+    cart.appendChild(img);
+    cart.appendChild(title);
+    cart.appendChild(description);
+    cart.appendChild(button);
+    container.appendChild(cart);
+  }
+};
+
+if (document.querySelector('.nav-link.active').textContent === 'Home')
+  fetchData(generateTrendingPlaces);
+else if (
+  document.querySelector('.nav-link.active').textContent === 'Holidays List'
+) {
+  console.log(document.querySelector('.nav-link.active').textContent);
+} else {
+  fetchData(generateAvailablePlaces);
+}
